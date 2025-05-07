1f0451d694479eedda805a4e47748c31e09d9695
petezahv4 <petezahiscool@gmail.com>
2025-05-07 16:50:44 -0400

    Bug fix and api redo

--- pages/other/ai/assets/js/index.js
+++ pages/other/ai/assets/js/index.js
@@ -505,7 +505,7 @@ sendMsg.addEventListener("click", () => {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": "Bearer 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"
+      "Authorization": "Bearer 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"
     },
     body: JSON.stringify(payload),
     signal: abortController.signal
